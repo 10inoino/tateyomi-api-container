f11255bc2e3874c83484398129c95058bd6a9e3f
Hikaru Inomata <inomatahi@icloud.com>
2021-07-31 12:38:11 +0000

    create demo modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoController } from './demo/demo.controller';
+import { DemoModule } from './demo/demo.module';
 
 @Module({
-  imports: [],
+  imports: [DemoModule],
   controllers: [AppController, DemoController],
   providers: [AppService],
 })
