4004db9f3d27cc09b395611bdd7ea5c38956e203
Hikaru Inomata <inomatahi@icloud.com>
2021-08-09 07:38:16 +0000

    change import place

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DemoController } from './demo/demo.controller';
 import { DemoModule } from './demo/demo.module';
-import { DemoService } from './demo/demo.service';
 
 @Module({
   imports: [DemoModule],
-  controllers: [AppController, DemoController],
-  providers: [AppService, DemoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 
 export class AppModule {}
