b85e0d7eb2b6eba59cd41f3dc556687ec2de5dcc
Hikaru Inomata <inomatahi@icloud.com>
2021-08-22 08:25:20 +0000

    modulesで読み込むクラス変更

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DemoService } from './demo.service';
 import { DemoController } from './demo.controller';
-import { DynamodbClient } from 'src/db/dynamodb.client';
+import { DemoRepository } from './demo.repository';
 
 // TODO:DI周りのドキュメントを読む：https://docs.nestjs.com/fundamentals/custom-providers
 @Module({
   controllers: [DemoController],
-  providers: [DemoService, DynamodbClient]
+  providers: [DemoService, DemoRepository]
 })
 export class DemoModule {}
