781bbe2eaf4b2ed3f9b2bdd230d23c8436d79345
Hikaru Inomata <inomatahi@icloud.com>
2021-09-05 10:47:13 +0000

    エラー箇所探索の為コメントアウト

--- src/demo/demo.controller.ts
+++ src/demo/demo.controller.ts
@@ -1,10 +1,10 @@
 import {
-  Body,
+  // Body,
   Controller,
   Get,
-  NotFoundException,
-  Param,
-  Post,
+  // NotFoundException,
+  // Param,
+  // Post,
 } from '@nestjs/common';
 import { DemoService } from './demo.service';
 import { DemoRequestPost } from './demo.request.post';
@@ -13,18 +13,24 @@ import { DemoRequestPost } from './demo.request.post';
 export class DemoController {
   constructor(private readonly demoService: DemoService) {}
   
-  @Post()
-  async createUser(@Body() body: DemoRequestPost): Promise<any> {
-    const user = await this.demoService.createUser(body.toUser());
+  // @Post()
+  // async createUser(@Body() body: DemoRequestPost): Promise<any> {
+  //   const user = await this.demoService.createUser(body.toUser());
     
-    return { status: true, data: user };
-  }
-  @Get(':id')
-  async getUserById(@Param() { id }: any): Promise<any> {
-    const user = await this.demoService.getUserById(id);
-    if (!user) {
-      throw new NotFoundException(`User with ID "${id}" not found`);
-    }
-    return { status: true, data: user };
-  }
+  //   return { status: true, data: user };
+  // }
+  
+  // @Get(':id')
+  // async getUserById(@Param() id : string ): Promise<any> {
+  //   const user = await this.demoService.getUserById(id);
+  //   if (!user) {
+  //     throw new NotFoundException(`User with ID "${id}" not found`);
+  //   }
+  //   return { status: true, data: user };
+  // }
+
+  @Get('hello')
+  getHello(): string {
+    return this.demoService.getHello();
+  }  
 }
--- src/demo/demo.service.ts
+++ src/demo/demo.service.ts
@@ -5,13 +5,17 @@ import { DemoRepository } from './demo.repository';
 
 @Injectable()
 export class DemoService {
-  constructor(private readonly repository: DemoRepository) {}
+  // constructor(private readonly repository: DemoRepository) {}
 
-  async createUser(dto: User): Promise<User> {
-    return await this.repository.save(dto);
-  }
+  // async createUser(dto: User): Promise<User> {
+  //   return await this.repository.save(dto);
+  // }
 
-  async getUserById(id: string): Promise<User> {
-    return await this.repository.find(id);
+  // async getUserById(id: string): Promise<User> {
+  //   return await this.repository.find(id);
+  // }
+
+  getHello(): string {
+    return 'I am demo!';
   }
 }
