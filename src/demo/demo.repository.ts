8a74e36a621fc5833e5770ded0e16086db6486cd
Hikaru Inomata <inomatahi@icloud.com>
2021-09-20 10:38:22 +0000

    DIができたので、repositoryからdynamodbの依存関係を排除

--- src/demo/demo.repository.ts
+++ src/demo/demo.repository.ts
@@ -1,14 +1,10 @@
 import { User } from '../entity/user';
 import { Env } from '../env';
 import { DatabaseClientInterface, DATABASE_CLIENT } from '../db/database.client.interface';
-import { DynamodbClient } from '../db/dynamodb.client'; 
 import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DemoRepository {
-  
-  // こうすると具象に依存してしまう
-  // constructor(private readonly DynamodbClient : DynamodbClient) {};
 
   // 本来はDynamoBD以外でも使うので、こうしたい
   constructor(@Inject(DATABASE_CLIENT) private readonly db : DatabaseClientInterface) {};
