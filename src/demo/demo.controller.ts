a30f823a942d295b8b4eb0e64b013d6bd076a61a
Hikaru Inomata <inomatahi@icloud.com>
2021-09-23 04:33:00 +0000

    findの引数としてhelloが取られている可能性があるため、getをコメントアウト

--- src/demo/demo.controller.ts
+++ src/demo/demo.controller.ts
@@ -7,27 +7,27 @@ import {
   Post,
 } from '@nestjs/common';
 import { DemoService } from './demo.service';
-// import { DemoRequestPost } from './demo.request.post';
+import { DemoRequestPost } from './demo.request.post';
 
 @Controller('demo')
 export class DemoController {
   constructor(private readonly demoService: DemoService) {}
   
-  // @Post()
-  // async createUser(@Body() body: DemoRequestPost): Promise<any> {
-  //   const user = await this.demoService.createUser(body.toUser());
+  @Post()
+  async createUser(@Body() body: DemoRequestPost): Promise<any> {
+    const user = await this.demoService.createUser(body.toUser());
     
-  //   return { status: true, data: user };
-  // }
-  
-  @Get(':id')
-  async getUserById(@Param() id : string ): Promise<any> {
-    const user = await this.demoService.getUserById(id);
-    if (!user) {
-      throw new NotFoundException(`User with ID "${id}" not found`);
-    }
     return { status: true, data: user };
   }
+  
+  // @Get(':id')
+  // async getUserById(@Param() id : string ): Promise<any> {
+  //   const user = await this.demoService.getUserById(id);
+  //   if (!user) {
+  //     throw new NotFoundException(`User with ID "${id}" not found`);
+  //   }
+  //   return { status: true, data: user };
+  // }
 
   @Get('hello')
   getHello(): string {
